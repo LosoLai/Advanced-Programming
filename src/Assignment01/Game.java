8c43b68cc5462b4fd0b0d4e008eed28a605272be
rmit-s3579161-loso-lai <s3579161@student.rmit.edu.au>
2017-03-21 13:04:32 +1100

    adding process part for each function:
    Add main() to deal with menu validation

--- src/Assignment01/Game.java
+++ src/Assignment01/Game.java
@@ -1,16 +1,19 @@
 package Assignment01;
 
 public abstract class Game {
+	public static int gameRoundNum = 0;
 	private String gameType;
 	
 	public Game()
 	{
 		this.gameType = "";
+		this.gameRoundNum = 0;
 	}
 	
 	public Game(String gameType)
 	{
 		this.gameType = gameType;
+		this.gameRoundNum++;
 	}
 	
 	abstract public int generateTime();
