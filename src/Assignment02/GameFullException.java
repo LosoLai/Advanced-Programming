7e1bb6a867c3c0a0eea2dbedd0ff81549aa1371f
Arion <s3650046@student.rmit.edu.au>
2017-05-15 01:38:34 +1000

    Added TooManyRefereeException extends RuntimeException Changed GameFullException to extend RuntimeException

--- src/Assignment02/GameFullException.java
+++ src/Assignment02/GameFullException.java
@@ -3,7 +3,7 @@ package Assignment02;
 /**@Author Arion
  * 
  */
-public class GameFullException extends Exception {
+public class GameFullException extends RuntimeException {
 	
 	public GameFullException(String errMsg) {
 		super(errMsg);
---
+++ src/Assignment02/TooManyRefereeException.java
@@ -0,0 +1,11 @@
+package Assignment02;
+
+/**@Author Arion
+ * 
+ */
+public class TooManyRefereeException extends RuntimeException{
+
+	public TooManyRefereeException(String errMsg) {
+		super(errMsg);
+	}
+}
